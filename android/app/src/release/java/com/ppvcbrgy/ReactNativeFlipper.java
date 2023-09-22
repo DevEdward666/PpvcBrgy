fb37c4adbbc52a7a7068041da30cf6476e4bf294
devEdward666 <edwardjosephfernandez@gmail.com>
2023-09-23 01:56:33 +0800

    remove class.forName in ReactNativeFlipper

--- android/app/src/release/java/com/ppvcbrgy/ReactNativeFlipper.java
+++ android/app/src/release/java/com/ppvcbrgy/ReactNativeFlipper.java
@@ -16,6 +16,6 @@ import com.facebook.react.ReactInstanceManager;
 public class ReactNativeFlipper {
   public static void initializeFlipper(Context context, ReactInstanceManager reactInstanceManager) {
     // Do nothing as we don't want to initialize Flipper on Release.
-    Class<?> aClass = Class.forName("com.ppvcbrgy");
+    // Class<?> aClass = Class.forName("com.ppvcbrgy");
   }
 }
